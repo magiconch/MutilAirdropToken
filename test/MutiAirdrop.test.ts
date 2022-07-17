74db6b88406da6a3ba4f55df8b621a2ee1085ea0
magiconch <magiconch@outlook.com>
2022-07-17 19:02:27 +0800

    Update unittest

--- test/MutiAirdrop.test.ts
+++ test/MutiAirdrop.test.ts
@@ -2,21 +2,25 @@ import { expect } from "chai";
 import { Contract, Signer } from "ethers";
 import { ethers } from "hardhat";
 
-describe("MutiAirdrop", function () {
+describe("MutiAirdrop", async function () {
   let mutiAirdrop: Contract;
   let deployer: Signer;
   let accounts: Signer[];
   beforeEach("Deploy", async () => {
+    console.log("Start Deploy");
     accounts = await ethers.getSigners();
     deployer = accounts[0];
     const MutiAirdrop = await ethers.getContractFactory("MultAirdrop", deployer);
     mutiAirdrop = await MutiAirdrop.deploy();
+    console.log("Deployed MutiAirdrop to:", mutiAirdrop.address);
   });
 
-  describe("Access", function () {
+  describe("Access", async function () {
+    // const adminRole = mutiAirdrop.DAO_ADMIN();
+    // const agentRole = mutiAirdrop.DAO_AGENT();
     it("Deployer should get admin role", async function () {
-      const flag = await mutiAirdrop.hasRole(mutiAirdrop.DAO_ADMIN(), deployer.getAddress());
-      console.log(flag);
+      const result = await mutiAirdrop.hasRole(mutiAirdrop.DAO_ADMIN(), deployer.getAddress());
+      expect(result).to.be.true;
     });
   });
 });
