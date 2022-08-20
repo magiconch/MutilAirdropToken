bf72884f71a75cbd7f52eac3bb22cccb9012cc0c
magiconch <magiconch@outlook.com>
2022-08-20 18:20:15 +0800

    Update unittest

--- test/MutiAirdrop.test.ts
+++ test/MutiAirdrop.test.ts
@@ -2,17 +2,17 @@ import { expect } from "chai";
 import { Contract, Signer } from "ethers";
 import { ethers } from "hardhat";
 
+const EVALUE = ethers.utils.parseEther("1");
+
 describe("MutiAirdrop", async function () {
   let mutiAirdrop: Contract;
   let deployer: Signer;
   let accounts: Signer[];
   beforeEach("Deploy", async () => {
-    console.log("Start Deploy");
     accounts = await ethers.getSigners();
     deployer = accounts[0];
     const MutiAirdrop = await ethers.getContractFactory("MultAirdrop", deployer);
     mutiAirdrop = await MutiAirdrop.deploy();
-    console.log("Deployed MutiAirdrop to:", mutiAirdrop.address);
   });
 
   describe("Access", async function () {
@@ -30,4 +30,37 @@ describe("MutiAirdrop", async function () {
       expect(result).to.be.true;
     });
   });
+
+  describe("Invest & withdraw", async function () {
+    // const adminRole = mutiAirdrop.DAO_ADMIN();
+    // const agentRole = mutiAirdrop.DAO_AGENT();
+    it("Contract should invested eth", async function () {
+      
+      await deployer.sendTransaction({
+        to: mutiAirdrop.address,
+        value: EVALUE,
+      });
+
+      const result = await mutiAirdrop.getBalance();
+      expect(result.toString()).to.be.equal(EVALUE.toString());
+
+      await mutiAirdrop.withdraw();
+
+      const result2 = await mutiAirdrop.getBalance();
+      expect(result2.toString()).to.be.equal("0");
+
+    });
+
+    it("Contract should invested coin", async function () {
+      // const otherUser = accounts[1].getAddress();
+      // await mutiAirdrop.grantRole(mutiAirdrop.DAO_ADMIN(), otherUser);
+      // const result = await mutiAirdrop.hasRole(mutiAirdrop.DAO_ADMIN(), deployer.getAddress());
+      // expect(result).to.be.true;
+      // Hi there!
+      
+      // 
+    });
+  });
+
+
 });
